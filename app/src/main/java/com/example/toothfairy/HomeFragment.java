e1db01a3ff5ac03f604709652964e20ddcce2f39
donghyeon0915 <dongdong0915_@naver.com>
2022-07-06 02:41:10 +0900

    UPDATE : 홈 화면 요소 추가

--- app/src/main/java/com/example/toothfairy/HomeFragment.java
+++ app/src/main/java/com/example/toothfairy/HomeFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +24,8 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    ProgressBar progressBar;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -59,6 +62,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // 프로그래스바 초기화
+        progressBar = view.findViewById(R.id.profileProgressBar);
+        progressBar.setProgress(60);
+        
+        return view;
     }
 }
